62d7f4fbcaa05628126b4a7868ea8591cc855c4e
EZEN3\EZENIC-213T <ashleyriot000@gmail.com>
2026-02-11 13:56:30 +0900

    비교연산자, 증감연산자, 논리연산자, if, for

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class Program
     {
+        public enum AAA
+        {
+            가위 = 0,
+            바위 = 1,
+            보 = 2
+        }
         static void Main(string[] args)
         {
             //Console.Write("당신의 이름은 무엇입니까?");
@@ -76,9 +82,176 @@
 
             //int sum = integers[0] + integers[1] + integers[2];
             //float avg = sum / integers.Length;
-            
+
             //2차원 배열            
-            //int[,] scores = new int[3, 10];
+            //int[,] counts = new int[3, 10];
+
+            //int[] scores = new int[3];
+            //Console.Write("당신의 국어점수를 입력해주세요 : ");
+            //scores[0] = int.Parse(Console.ReadLine());
+            //Console.Write("당신의 영어점수를 입력해주세요 : ");
+            //scores[1] = int.Parse(Console.ReadLine());
+            //Console.Write("당신의 수학점수를 입력해주세요 : ");
+            //scores[2] = int.Parse(Console.ReadLine());
+            //float average = (scores[0] + scores[1] + scores[2]) / (float)scores.Length;
+            //Console.WriteLine("당신의 평균 점수는 {0:f2}입니다.");
+
+            int iD = 1;
+
+            //증감 연산자
+            //Console.WriteLine("iD의 값은 {0}", iD++);
+
+            //iD += 1;
+            //iD = iD + 1;
+            //Sum(1, 5);
+
+            //Console.Write("두 개의 자연수 중에서 큰 수를 알아냅니다.\n첫번째 자연수를 입력해주세요 :");         
+            //InputIntegers(out int first, out int second);
+            //Console.Write("둘 중 더 큰 자연수는 {0}입니다", Max(first, second));
+
+            //Console.Write("두 개의 자연수 중에서 작은 수를 알아냅니다.\n첫번째 자연수를 입력해주세요 :");
+            //InputIntegers(out first, out second);
+            //Console.Write("둘 중 더 작은 자연수는 {0}입니다", Min(first, second));
+
+            /*
+            int iE = 5;
+            int iF = 10;
+            int iG = 11;
+            if(iE == iF)
+            {
+                Console.WriteLine("iE와 iF는 같습니다.");
+            }
+            else
+            {
+                Console.WriteLine("iE와 iF는 다릅니다.");
+            }           
+
+            if (iE > iF)
+            {
+                Console.WriteLine("iE가 더 큽니다.");
+            }
+            else if(iE == iF)
+            {
+                Console.WriteLine("iE는 iF와 같습니다.");
+            }
+            else
+            {
+                Console.WriteLine("iE가 더 작습니다.");
+            }
+            */
+
+            //if(!(iE == iF) & iE > iG )
+            //{
+            //    Console.WriteLine("iE와 iF가 다르고 iE가 iG보다 큽니다.");
+            //}
+
+            //if(iE == iG | iE < iG)
+            //{
+            //    Console.WriteLine("iE와 iF가 같을수도 있고 iE가 iG보다 작을 수도 있습니다.");
+            //}
+
+            Console.WriteLine("가위바위보 게임 스타트");
+            Console.Write("게임을 몇번 하시겠습니까? : ");
+            int count = int.Parse(Console.ReadLine());
+            for(int i = 0; i < count; i++)
+            {
+                Game();
+            }
         }
+
+
+        /// <summary>
+        /// 두개의 정수를 더한다.
+        /// </summary>
+        /// <param name="a">첫번째 정수</param>
+        /// <param name="b">두번째 정수</param>
+        /// <returns>결과값</returns>
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Run");
+        }
+        /// <summary>
+        /// 둘 중에 더 큰 값을 반환한다.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static int Max(int a, int b)
+        {
+            if(a == b) return a;
+            if(a > b)  return a;
+            return b;
+        }
+
+        static int Min(int a, int b)
+        {
+            if (a == b) return a;
+            if (a < b) return a;
+            return b;
+        }
+
+        static void InputIntegers(out int first, out int second)
+        {
+            first = int.Parse(Console.ReadLine());
+            Console.Write("첫번째 자연수를 입력해주세요 :");
+            second = int.Parse(Console.ReadLine());
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.Write(".");
+            Thread.Sleep(500);
+            Console.WriteLine(".");
+        }
+
+        static void Game()
+        {
+            Console.Write("내실 가위바위보를 정해 입력해 주세요(0:가위, 1:바위, 2:보) : ");
+            Random random = new Random();
+            int com = random.Next(0, 3);
+            int player = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("당신은 {0}을 냈고, 컴퓨터는 {1}을 냈습니다", (AAA)player, (AAA)com);
+            //서로 같은 걸 낼 경우
+            if (com == player)
+            {
+                Console.WriteLine("서로 비겼습니다.");
+            }
+            //서로 다른 걸 낼 경우, 컴퓨터가 가위를 낼 경우
+            else if (com == 0)
+            {
+                //플레이어가 보를 낼 경우 컴퓨터가 이겼음 표시하기.
+                if (player == 2)
+                    Console.WriteLine("당신이 졌습니다.");
+                //플레이어가 바위 플레이어가 이겼음 표시하기.
+                else
+                    Console.WriteLine("당신이 이겼습니다.");
+            }
+            //서로 다른 걸 낼 경우,  컴퓨터가 바위
+            else if (com == 1)
+            {
+                //플레이어가 보를 낼 경우
+                if (player == 2)
+                    Console.WriteLine("당신이 이겼습니다.");
+                //플레이어가 가위
+                else
+                    Console.WriteLine("당신이 졌습니다.");
+            }
+            //컴퓨터가 보
+            else
+            {
+                //플레이어가 가위를 낼 경우
+                if (player == 0)
+                    Console.WriteLine("당신이 졌습니다.");
+                //플레이어가 바위
+                else
+                    Console.WriteLine("당신이 이겼습니다.");
+            }
+        }
+
     }
 }
--- CSharpStudy/obj/Debug/net10.0/CSharpStudy.AssemblyInfo.cs
+++ CSharpStudy/obj/Debug/net10.0/CSharpStudy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpStudy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81a0d957821d1ed474ce298f168eddd213acc65")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpStudy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpStudy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
